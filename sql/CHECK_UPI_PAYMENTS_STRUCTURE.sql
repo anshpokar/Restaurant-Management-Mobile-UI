e50181b3876d1af387c5bbac034b621aff6dcf8e
Ansh Jagdish Pokar <anshjpokar@gmail.com>
2026-04-04 10:25:10 +0530

    removing teh unwanted files

--- sql/CHECK_UPI_PAYMENTS_STRUCTURE.sql
+++
@@ -1,6 +0,0 @@
--- Check the actual structure of upi_payments table
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns
-WHERE table_schema = 'public' 
-AND table_name = 'upi_payments'
-ORDER BY ordinal_position;
